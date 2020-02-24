286a4a37129d0df17bdfa4990630c8bc7a5a2ad4
kimberlybone <k_bone@uncg.edu>
2020-02-24 15:34:23 -0500

    day 1 solution

---
+++ 30_Day_Challenge/Day_1.js
@@ -0,0 +1,35 @@
+function main() {
+
+  var i = 4
+  var d = 4.0
+  var s = "HackerRank "
+  
+  // Declare second integer, double, and String variables.
+  var x = "";
+
+  var y = "";
+
+  var z = "";
+
+  // Read and save an integer, double, and String to your variables.
+
+  x = parseInt(readLine()) + i;
+
+  y = parseFloat(readLine()) + d;
+
+  z = s + readLine();
+
+  // Print the sum of both integer variables on a new line.
+
+  console.log(x);
+
+  // Print the sum of the double variables on a new line.
+
+  console.log(y.toFixed(1));
+
+  // Concatenate and print the String variables on a new line
+
+  // The 's' variable above should be printed first.
+
+  console.log(z);
+}
